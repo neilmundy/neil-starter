6726ca35e2491ddcc15adf7b280f82d7d1e4780c
Neil Mundy <neil@nem.so>
2023-11-29 11:34:37 +0000

    First deploy

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
 (async () => {
   console.log("I an the front end");
-})();
+}
+// ...
+paypal
+.Buttons({
+    createOrder() {
+        return getOrderId();
+    },
+    onApprove: async function (data) {
+        handlePaymentApproval();
+  
+    fetch("/pay", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+      
+      }),
+
+  })
+  .render('#paypal-button-container');
+// ...
+)();
--- server.js
+++ server.js
@@ -5,7 +5,44 @@ app.use(express.static("public"));
 app.use(express.json());
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 
+
+const PROCESSING_CHANNEL_ID = "pc_eb7s65gqhy5e3ig3xegfdlyaee";
+const SECRET_KEY = "sk_sbox_f53xg6w5x4fzcyziyujfmmrh5u3";
+
 // Write your code bellow. Here is an example of a route:
-app.post("/ping", async (req, res) => {
-  res.send("pong");
+app.post("/create-context", async (req, res) => {
+  const paymentResponse = await fetch(
+    "https://api.sandbox.checkout.com/payment-contexts",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        source: {
+          type: "paypal",
+        },
+        currency: "GBP",
+        amount: 2000,
+        capture: true,
+        items: [
+          {
+          name: "laptop",
+          unit_price: 2000,
+          quantity: 1
+          }
+         ],
+        processing_channel_id: PROCESSING_CHANNEL_ID,
+        success_url: "http://www.example.com/success.html",
+        failure_url: "http://www.example.com/failure.html",
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
+
+  res.send(paymentResponse);
 });
+
+
